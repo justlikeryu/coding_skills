d452a3ada9ec7d77a2c5068af4740483295dda40
RYU <111476710+justlikeryu@users.noreply.github.com>
2024-01-31 12:39:32 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        for(int i = 0; i < common.length; i++){
+            if(common[1] - common[0] == common[2] - common[1]){
+                answer = common[common.length - 1] + (common[1] - common[0]);
+            } else if (common[1] / common[0] == common[2] / common[1]){
+                answer = common[common.length - 1] * (common[1] / common[0]);
+            }
+        }
+        
+        return answer;
+    }
+}
