ef46b17ae3126d124ea5a6424dfa87e4c354eed4
RYU <111476710+justlikeryu@users.noreply.github.com>
2024-02-19 21:25:37 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        scanner.close();
+        
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+}
