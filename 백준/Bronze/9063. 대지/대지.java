888e1fd087126f2c2056bf664d818403fec22331
RYU <111476710+justlikeryu@users.noreply.github.com>
2024-03-19 23:50:39 +0900

    [Bronze III] Title: 대지, Time: 296 ms, Memory: 27456 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int xMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int yMin = Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            xMin = Math.min(xMin, x);
+            xMax = Math.max(xMax, x);
+            yMin = Math.min(yMin, y);
+            yMax = Math.max(yMax, y);
+        }
+
+        int area = (xMax - xMin) * (yMax - yMin);
+        if (area < 0) {
+            area = 0;
+        }
+        bw.write(String.valueOf(area));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
