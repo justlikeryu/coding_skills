6deccbc48f783bf7cb32709d1592aaf38914dff8
RYU <111476710+justlikeryu@users.noreply.github.com>
2024-04-12 21:49:28 +0900

    [Silver IV] Title: 제로, Time: 264 ms, Memory: 24116 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                stack.push(num);
+            } else {
+                stack.pop();
+            }
+        }
+        br.close();
+
+        int sum = 0;
+        for(int x : stack){
+            sum += x;
+        }
+
+        bw.write(sum +"");
+
+        bw.flush();
+        bw.close();
+    }
+}
